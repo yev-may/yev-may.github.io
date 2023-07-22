0ee4b8669511d27ad55836f20af86b5b39aef279
yevay <yevhenii.may@gmail.com>
2023-07-22 13:42:40 +0300

    Split app to tabs

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,29 @@
 <template>
-  <CardForm />
-  <CardsView />
-  <Repetition />
+  <div>
+    <button @click="activateTab('repetition')">Repetition</button>
+    <button @click="activateTab('addCard')">Add Card</button>
+    <button @click="activateTab( 'collection')">Collection</button>
+  </div>
+  <div>
+    <Repetition v-if="isActiveTab('repetition')"/>
+    <CardForm v-if="isActiveTab('addCard')"/>
+    <CardsView v-if="isActiveTab('collection')"/>
+  </div>
 </template>
+
 <script setup>
 import CardForm from "./CardForm.vue";
 import CardsView from "./CardsView.vue";
 import Repetition from "./Repetition.vue";
+import {ref} from "vue";
+
+const activeTap = ref("repetition");
+
+function activateTab(tabName) {
+  activeTap.value = tabName;
+}
+
+function isActiveTab(tabName) {
+  return activeTap.value === tabName;
+}
 </script>
