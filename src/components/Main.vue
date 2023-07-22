c90826c70da0d111e971a022fd8f7082804f3294
yevay <yevhenii.may@gmail.com>
2023-07-22 22:04:18 +0300

    Ass some style

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <button @click="activateTab('repetition')">Repetition</button>
-    <button @click="activateTab('addCard')">Add Card</button>
-    <button @click="activateTab( 'collection')">Collection</button>
+    <header>
+      <div class="header_container">
+        <button @click="activateTab('repetition')">Repetition</button>
+        <button @click="activateTab('addCard')">Add Card</button>
+        <button @click="activateTab( 'collection')">Collection</button>
+      </div>
+    </header>
   </div>
   <div>
     <Repetition v-if="isActiveTab('repetition')"/>
@@ -27,3 +31,61 @@ function isActiveTab(tabName) {
   return activeTap.value === tabName;
 }
 </script>
+
+<style>
+body {
+  margin: 0;
+  font-size: 18px;
+  font-family: Arial, serif;
+}
+header {
+  background-color: black;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+}
+
+.header_container {
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.header_container button {
+  background-color: black;
+  border: 1px solid white;
+  color: white;
+  height: 40px;
+  font-size: 20px;
+  margin-right: 10px;
+}
+.header_container button:last-child {
+  margin-right: 0
+}
+.header_container button:hover {
+  background-color: lightgray;
+}
+.header_container button:active {
+  background-color: white;
+  border: 1px solid black;
+}
+
+button {
+  margin: 0;
+  border: 1px solid black;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+}
+button:hover {
+  background-color: lightgray;
+  color: black;
+}
+button:active {
+  border: 1px solid white;
+  background-color: white;
+  color: black;
+}
+</style>
