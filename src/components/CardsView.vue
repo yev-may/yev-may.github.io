834dde6f6c2cc5a7c53755a33b8859f71b23a6e5
yevay <yevhenii.may@gmail.com>
2023-07-24 19:51:03 +0300

    Add cards pagination

--- src/components/CardsView.vue
+++ src/components/CardsView.vue
@@ -1,8 +1,16 @@
 <script setup>
-import {reactive, ref} from "vue";
+import {computed, reactive, ref} from "vue";
 import {deleteCard, getCards, updateCard} from "../module/cardService";
 
-const cards = ref(getCards().sort((c1, c2) => c1.creationDate - c2.creationDate));
+const cards = ref(getCards());
+// Pagination
+const pageSize = ref(7);
+const pageSelected = ref(0);
+const totalPages = computed(() => Math.ceil(cards.value.length / pageSize.value));
+const pageContent = computed(() => cards.value.slice(
+    pageSelected.value * pageSize.value,
+    pageSelected.value * pageSize.value + pageSize.value));
+// Edit
 const editCardForm = reactive({
   id: null,
   question: "",
@@ -26,6 +34,7 @@ function updateCards() {
 function confirmCardUpdate() {
   updateCard(editCardForm);
   updateCards()
+  cancelCardUpdate();
 }
 
 function confirmCardRemoving(cardId) {
@@ -42,22 +51,32 @@ function formatQuestionPreview(question) {
   <div class="view">
     <div class="view_container">
 
-      <div class="card_preview" v-for="card in cards">
-        <div class="card_preview_text">
-          {{ formatQuestionPreview(card.question) }}
+      <div class="card-view" v-if="!editCardForm.id">
+        <div class="card_preview" v-for="card in pageContent">
+          <div class="card_preview_text">
+            {{ formatQuestionPreview(card.question) }}
+          </div>
+          <div class="card_preview_actions">
+            <button class="action_button" @click="selectCardToEdit(card)">E</button>
+            <button class="action_button" @click="confirmCardRemoving(card.id)">X</button>
+          </div>
         </div>
-        <div class="card_preview_actions">
-          <button class="action_button" @click="selectCardToEdit(card)">E</button>
-          <button class="action_button" @click="confirmCardRemoving(card.id)">X</button>
+
+        <div class="pagination-controller">
+          <button class="pagination-button"
+                  v-for="index in totalPages"
+                  @click="pageSelected = index - 1">
+            {{ index }}
+          </button>
         </div>
       </div>
 
       <div class="edit-card_form" v-if="editCardForm.id">
         <p>Question:</p>
-        <textarea rows="3" v-model="editCardForm.question" />
+        <textarea rows="3" v-model="editCardForm.question"/>
 
         <p>Answer:</p>
-        <textarea rows="3" v-model="editCardForm.answer" />
+        <textarea rows="3" v-model="editCardForm.answer"/>
 
         <div class="inline-blocks">
           <button @click="confirmCardUpdate()">Save</button>
@@ -110,5 +129,16 @@ function formatQuestionPreview(question) {
 .edit-card_form button {
   display: inline-block;
   width: 245px;
+}
+
+.pagination-controller {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.pagination-button {
+  height: 50px;
+  width: 50px;
+  margin: 5px;
 }
 </style>
