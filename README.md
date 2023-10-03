7a7a13c418b14c5817cbd9c59b538d7effe4e54e
yevay <yevhenii.may@gmail.com>
2023-10-03 11:59:10 +0300

    release-23-10-03

--- README.md
+++
@@ -1,7 +0,0 @@
-# Remiry Light
-
----
-
-Web application for studying with
-[Spaced Repetition](https://en.wikipedia.org/wiki/Spaced_repetition)
-technique
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import PrimeVue from "primevue/config";
-import 'primeicons/primeicons.css';
-
-createApp(App)
-    .use(PrimeVue)
-    .mount('#app')
